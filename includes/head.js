c76840d82a68aab9ca441a4bcf7b0d1134530c53
sjvl <samueljavelle@hotmail.com>
2020-11-04 15:10:37 +0100

    commit

--- includes/head.js
+++ includes/head.js
@@ -1,7 +1,7 @@
 include( `
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>makers' lab</title>
+    <title>aescra</title>
 
     <!-- CSS -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
