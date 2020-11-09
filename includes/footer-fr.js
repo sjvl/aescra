e7c2d4a794876c0532682062bb2e08b8c350ff9c
sjvl <samueljavelle@hotmail.com>
2020-11-09 10:29:18 +0100

    add FR

--- includes/footer-fr.js
+++ includes/footer-fr.js
@@ -3,17 +3,17 @@ include( `
         <div class="container">
             <div class="row">
                 <div class="col m6">
-                    <h5 class="white-text">About us</h5>
+                    <h5 class="white-text">À propos</h5>
 
                     <p class="grey-text text-lighten-4">
-                    AESCRA is a non-profit association governed by the law of 1st July 1901.<br>Its purpose is to organize and manage in France and abroad professional training actions allowing the development of key skills in demand on the job market.
+                    L'AESCRA est une association à but non lucratif régie par la loi du 1er juillet 1901.<br>Elle a pour objet d'organiser et de gérer, en France et à l'étranger, des actions de formation professionnelle permettant le développement de compétences clés recherchées sur le marché du travail.
                     </p>
                 </div>
 
                 <div class="col m3"></div>
 
                 <div class="col m3" style="text-align:right;">
-                    <h5 class="white-text">Follow us</h5>
+                    <h5 class="white-text">Suivez-nous</h5>
                     <ul>
                         <li><a target="_blank" class="white-text" href="mailto:contact@aescra.com">Mail</a></li>
                         <li><a target="_blank" class="white-text" href="https://github.com/emlyon">Github</a></li>
